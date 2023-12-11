df0b0d8ad4d6b7b1239644b34fcce26595ef0e71
sitdown.liu <sitdown.liu@gmail.com>
2023-12-11 15:05:18 +0800

    v20231211 -senti-app-web：初始化【子应用】vue3-app

---
+++ senti-app-web/children/vue3-app/vue.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  outputDir: "dist",
+  assetsDir: "static",
+  indexPath: "index.html",
+  filenameHashing: true,
+  lintOnSave: false,
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    host: "0.0.0.0",
+    port: 34211,
+  },
+});
--- senti-app-web/src/app/app.module.ts
+++ senti-app-web/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DevUIModule } from 'ng-devui';
 import { FormDesignComponent } from './pages/form-design/form-design.component';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [AppComponent, FormDesignComponent],
   imports: [
     BrowserModule,
